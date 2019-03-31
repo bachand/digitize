37bcf0e9052a90ab7ac83e9db0d917e426dab6dc
Michael Bachand <bachand.michael@gmail.com>
2019-03-31 12:42:11 -0700

    start to fill out create method

--- lib/manifest.rb
+++ lib/manifest.rb
@@ -3,9 +3,23 @@ require 'json'
 class Manifest
 
   def create
+    conference = 'My Conference'
+    title = 'Tigers and Lions and Bears'
+    speakers = [
+      'Sylvester Stallone',
+      'Dolph Lundgren',
+    ]
+    data_paths = [
+      'TheRecording.dmg'
+    ]
+
     data = {
-      'manifest' => 'dummy'
+      'conference' => conference,
+      'title' => title,
+      'speakers' => speakers,
+      'data_paths' => data_paths,
     }
+
     puts JSON.generate(data)
   end
 end
