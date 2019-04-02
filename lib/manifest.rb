597e1360a5d3d956e99ea9da66ae347f0f143dd4
Michael Bachand <bachand.michael@gmail.com>
2019-04-01 23:38:16 -0700

    Collect all fields with prompting

--- lib/manifest.rb
+++ lib/manifest.rb
@@ -6,21 +6,24 @@ class Manifest
   def create_via_prompt
     prompt = TTY::Prompt.new
     conference = prompt.ask('What is the conference name?')
-    create(conference)
+    title = prompt.ask('What is the title of the recording?')
+    speakers = []
+    begin
+      speaker = prompt.ask('Who spoke at the conference?')
+      speakers << speaker
+    end while prompt.yes?("continue?", default: false)
+    data_paths = []
+    begin
+      data_path = prompt.ask('What are the data paths?')
+      data_paths << data_path
+    end while prompt.yes?("continue?", default: false)
+
+    create(conference, title, speakers, data_paths)
   end
 
   private
 
-  def create(conference)
-    title = 'Tigers and Lions and Bears'
-    speakers = [
-      'Sylvester Stallone',
-      'Dolph Lundgren',
-    ]
-    data_paths = [
-      'TheRecording.dmg'
-    ]
-
+  def create(conference, title, speakers, data_paths)
     data = {
       'conference' => conference,
       'title' => title,
