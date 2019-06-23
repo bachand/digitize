f87db2a4a578a4ec615fdfa323987146d35ba0d2
Michael Bachand <bachand.michael@gmail.com>
2019-06-22 17:27:58 -0700

    improve handbrake class, and get num_chapters working

--- lib/handbrake.rb
+++ lib/handbrake.rb
@@ -10,7 +10,8 @@ class HandBrake
   # Returns the number of chapters in a video.
   def num_chapters(path)
     escaped_path = Shellwords.escape(path)
-    command = "#{HANDBRAKE_PATH} --scan -i #{escaped_path}"
+    arguments = "--scan -i #{escaped_path}"
+    command = shell_command(arguments)
     Shell.output(command).match(/DVD has ([0-9]+) title/).captures.first.to_i
   end
 
@@ -22,14 +23,23 @@ class HandBrake
     escaped_preset_path = Shellwords.escape(preset_path)
 
     # --main-feature tells Handbrake to find the title that is the main feature and encode that.
-    command = %{
-#{HANDBRAKE_PATH} \
-  --preset-import-file #{escaped_preset_path} \
-  --main-feature \
-  -Z "#{preset_name}" \
-  -i #{escaped_source_path} \
-  -o #{escaped_output_path} \
-    }
+    arguments = <<~EOT
+    --preset-import-file #{escaped_preset_path} \
+    --main-feature \
+    -Z "#{preset_name}" \
+    -i #{escaped_source_path} \
+    -o #{escaped_output_path}
+    EOT
+    command = shell_command(arguments)
     Shell.run(command)
+  end
+
+  private
+
+  ##
+  # Creates a full command that can be run in the shell. Ensures that Handbrake prints to STDOUT. By
+  # default it seems to print to STDERR.
+  def shell_command(arguments)
+    "#{HANDBRAKE_PATH} #{arguments.strip} 2>&1"
   end
 end
