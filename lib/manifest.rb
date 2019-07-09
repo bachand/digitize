fbeb6d4152fcae441e60115c944f1bd5d81b4368
Michael Bachand <bachand.michael@gmail.com>
2019-07-08 22:08:12 -0700

    Allow manifest generating to make manifest with multiple titles

--- lib/manifest.rb
+++ lib/manifest.rb
@@ -6,28 +6,36 @@ class Manifest
   def create_via_prompt
     prompt = TTY::Prompt.new
     conference = prompt.ask('What is the conference name?')
-    title = prompt.ask('What is the title of the recording?')
-    speakers = []
-    begin
-      speaker = prompt.ask('Who spoke at the conference?')
-      speakers << speaker
-    end while prompt.yes?("continue?", default: false)
+    year = prompt.ask('What is the year of the conference?')
     data_paths = []
     begin
       data_path = prompt.ask('What are the data paths?')
       data_paths << data_path
-    end while prompt.yes?("continue?", default: false)
+    end while prompt.yes?("More data paths?", default: false)
+    titles = []
+    begin
+      title = prompt.ask('What is the title of the recording?')
+      speakers = []
+      begin
+        speaker = prompt.ask('Who spoke in this recording?')
+        speakers << speaker
+      end while prompt.yes?("More speakers?", default: false)
+      titles << {
+        'title' => title,
+        'speakers' => speakers,
+      }
+    end while prompt.yes?("More titles?", default: false)
 
-    create(conference, title, speakers, data_paths)
+    create(conference, year, titles, data_paths)
   end
 
   private
 
-  def create(conference, title, speakers, data_paths)
+  def create(conference, year, titles, data_paths)
     data = {
       'conference' => conference,
-      'title' => title,
-      'speakers' => speakers,
+      'year' => year,
+      'titles' => titles,
       'data_paths' => data_paths,
     }
 
